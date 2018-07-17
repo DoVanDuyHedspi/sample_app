c1573e8bbfbc5c3a51c19c62b9ff51c4288a662d
DoVanDuyHedspi <vanduy07c.r@gmail.com>
2018-07-17 09:41:34 +0700

    Chapter 13: user micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,10 @@ class UsersController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    @microposts = @user.microposts.page(params[:page])
+      .per(Settings.microposts.per_page)
+  end
 
   def edit; end
 
@@ -50,13 +53,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
-  end
-
-  def logged_in_user
-    return if logged_in?
-    store_location
-    flash[:danger] = t("logged_in_user")
-    redirect_to login_url
   end
 
   def correct_user
